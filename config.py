18f0c9f3e3dd42148052353c3d725056b647201b
EricKingWhy <hoeyw@outlook.com>
2025-12-28 13:14:37 +0800

    feat: 完成配置和Token获取模块

--- config.py
+++ config.py
@@ -1,8 +1,6 @@
 # config.py
-# 配置你的百度云密钥
-API_KEY = "GyqyRcadrpJqptsANO7SE86g"
-SECRET_KEY = "vTQK91cPqJXMzmzWlxxWzF1UHYtMakCN"
+API_KEY = "YOUR_API_KEY"
+SECRET_KEY = "YOUR_SECRET_KEY"
 
-# 接口地址
 TOKEN_URL = "https://aip.baidubce.com/oauth/2.0/token"
 OCR_URL = "https://aip.baidubce.com/rest/2.0/ocr/v1/general_basic"
